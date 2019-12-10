5a131cdebc85809c78eda60038349c6d5b41d93d
Zhi Li <zli2@ZLi2-m27.dhcp.lbnl.us>
2019-12-09 16:49:42 -0800

    solver completed, but needs debug

--- ns2d.py
+++ ns2d.py
@@ -1,6 +1,7 @@
 """ Main function of the ns2d solver """
 
 import numpy as np
+import matplotlib.pyplot as plt
 from map import *
 from variable import *
 
@@ -8,12 +9,15 @@ from variable import *
 #
 #   User settings
 #
+plot = True
 setting = {
-            'dim'       :   [4,5],
+            'dim'       :   [30,40],
             'delta'     :   [1.0,1.0],
-            'T'         :   [0.5,100],
-            'pBC'       :   [1100.0,1000.0],
+            'T'         :   [0.4,10,20],
+            'pBC'       :   [1.0,0.0],
             'pIC'       :   1000.0,
+            'sBC'       :   [1.0, 0.0],
+            'sIC'       :   0.0,
             'rho'       :   1000.0,
             'nu'        :   0.0001
 }
@@ -24,20 +28,52 @@ setting = {
 
 def main(setting):
     map = Map(setting)
-    data = Variable(setting)
-    data.enforceBoundary(map, setting)
+    data = Variable(map, setting)
+    data.enforcePressureBC(map, setting)
+    data.enforceVelocityBC(map, setting)
     for tstep in range(setting['T'][1]):
         #   Execute one time step
         data.explicitSource(map, setting)
         data.matrixRHS(map, setting)
         data.buildMatrix(map, setting)
         data.solve()
+        data.enforcePressureBC(map, setting)
         data.updateVelocity(map, setting)
-        data.enforceBoundary(map, setting)
+        data.enforceVelocityBC(map, setting)
         print('Time step ',str(tstep),' executed!')
+        # if tstep % setting['T'][2] == 0:
+        #     makePlot(data, setting)
     return data
-    
-main(setting)
-        
-        
-    
+
+#
+#   Plot function
+#
+def makePlot(data, setting):
+    p = np.transpose(np.reshape(data.pp[:data.Ni], (setting['dim'][1],setting['dim'][0])))
+    u = np.transpose(np.reshape(data.up[:data.Ni], (setting['dim'][1],setting['dim'][0])))
+    v = np.transpose(np.reshape(data.vp[:data.Ni], (setting['dim'][1],setting['dim'][0])))
+    s = np.transpose(np.reshape(data.sp[:data.Ni], (setting['dim'][1],setting['dim'][0])))
+    u_max = np.amax(abs(u))
+    v_max = np.amax(abs(v))
+    fig, ax = plt.subplots()
+    ax1 = plt.subplot(2,2,1)
+    plt.imshow(p, vmin=setting['pBC'][1], vmax=setting['pBC'][0])
+    cbar = plt.colorbar()
+    ax1 = plt.subplot(2,2,2)
+    plt.imshow(s, vmin=setting['sBC'][1], vmax=setting['sBC'][0])
+    cbar = plt.colorbar()
+    ax2 = plt.subplot(2,2,3)
+    plt.imshow(u, vmin=-u_max, vmax=u_max)
+    cbar = plt.colorbar()
+    ax3 = plt.subplot(2,2,4)
+    plt.imshow(v, vmin=-v_max, vmax=v_max)
+    cbar = plt.colorbar()
+    plt.show()
+
+#
+# Execution
+#
+if __name__=="__main__":
+    data = main(setting)
+    if plot == True:
+        makePlot(data, setting)
--- variable.py
+++ variable.py
@@ -1,14 +1,14 @@
 """ Create the data structure that stores the variables """
 import numpy as np
-from scipy.sparse import csc_matrix
+from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import spsolve
 
 class Variable():
-    def __init__(self, setting):
+    def __init__(self, map, setting):
         self.Nx = setting['dim'][0]
         self.Ny = setting['dim'][1]
-        self.dx = setting['dim'][0]
-        self.dy = setting['dim'][1]
+        self.dx = setting['delta'][0]
+        self.dy = setting['delta'][1]
         self.Ni = self.Nx * self.Ny
         self.Nt = (self.Nx+2) * (self.Ny+2)
         self.dt = setting['T'][0]
@@ -16,6 +16,8 @@ class Variable():
         self.nu = setting['nu']
         self.pBC = setting['pBC']
         self.pIC = setting['pIC']
+        self.sBC = setting['sBC']
+        self.sIC = setting['sIC']
         #   Variables that remain constants for now
         self.V = self.dx * self.dy
         self.Ax = self.dy
@@ -23,26 +25,33 @@ class Variable():
         #   Variables on cell centers
         self.pn = self.pIC * np.ones(self.Nt)
         self.pp = self.pIC * np.ones(self.Nt)
+        for ii in range(self.Nx):
+            jj = self.Nx * (self.Ny-1) + ii
+            self.pn[ii] = self.pBC[0]
+            self.pp[ii] = self.pBC[0]
+            self.pn[jj] = self.pBC[1]
+            self.pp[jj] = self.pBC[1]
+        # use linear initial Pressure
+        for ii in range(self.Ni):
+            col = map.col[ii]
+            self.pn[ii] = self.pBC[0] - col / (self.pBC[0]-self.pBC[1])
+        self.sn = self.sIC * np.ones(self.Nt)
+        self.sp = self.sIC * np.ones(self.Nt)
         self.B = np.zeros((self.Ni,1))
         #   Variables on cell faces
         self.un = np.zeros(self.Nt)
         self.up = np.zeros(self.Nt)
         self.vn = np.zeros(self.Nt)
         self.vp = np.zeros(self.Nt)
-        self.Gxp = (self.dt * self.Ax / (self.rho * self.V)) * np.ones(self.Ni)
-        self.Gxm = (self.dt * self.Ax / (self.rho * self.V)) * np.ones(self.Ni)
-        self.Gyp = (self.dt * self.Ax / (self.rho * self.V)) * np.ones(self.Ni)
-        self.Gym = (self.dt * self.Ax / (self.rho * self.V)) * np.ones(self.Ni)
-        self.Gct = self.Gxp + self.Gxm + self.Gyp + self.Gym
         #   Matrix coefficients
         self.Gx = self.dt * self.Ax / (self.rho * self.V)
         self.Gy = self.dt * self.Ay / (self.rho * self.V)
         self.Gc = 2.0 * self.Gx + 2.0 * self.Gy
-        
+
     def explicitSource(self, map, setting):
         """ Calculate the explicit terms """
-        self.Ex = np.zeros(self.Ni)
-        self.Ey = np.zeros(self.Ni)
+        self.Ex = np.zeros(self.Nt)
+        self.Ey = np.zeros(self.Nt)
         for ii in range(self.Ni):
             #   Explicit velocity
             self.Ex[ii] = self.un[ii]
@@ -57,42 +66,60 @@ class Variable():
             self.Ex[ii] += (self.dt * self.nu * self.Ay / (self.dy * self.V)) * (self.un[map.icjP[ii]] - 2.0*self.un[ii] + self.un[map.icjM[ii]])
             self.Ey[ii] += (self.dt * self.nu * self.Ax / (self.dx * self.V)) * (self.vn[map.iPjc[ii]] - 2.0*self.vn[ii] + self.vn[map.iMjc[ii]])
             self.Ey[ii] += (self.dt * self.nu * self.Ay / (self.dy * self.V)) * (self.vn[map.icjP[ii]] - 2.0*self.vn[ii] + self.vn[map.icjM[ii]])
-    
+        for ii in range(self.Nx):
+            self.Ey[map.icjM[ii]] = self.Ey[ii]
+        for ii in range(self.Ny):
+            jj = ii * self.Nx
+            self.Ex[map.iMjc[jj]] = self.Ex[jj]
+
     def matrixRHS(self, map, setting):
         """ Calculate right-hand-side of the linear system """
         self.B = np.zeros(self.Ni)
         for ii in range(self.Ni):
             self.B[ii] = -(self.Ex[ii] + self.Ex[map.iMjc[ii]] + self.Ey[ii] + self.Ey[map.icjM[ii]])
-            
+
     def updateVelocity(self, map, setting):
         """ Update velocity based on pressure """
         for ii in range(self.Ni):
             self.up[ii] = -self.Gx * (self.pp[map.iPjc[ii]] - self.pp[ii]) + self.Ex[ii]
             self.vp[ii] = -self.Gy * (self.pp[map.icjP[ii]] - self.pp[ii]) + self.Ey[ii]
-            
-    def enforceBoundary(self, map, setting):
-        """ Enforce BC on pressure and velocity """
+
+    def enforcePressureBC(self, map, setting):
+        """ Enforce BC on pressure """
         for ii in range(self.Nx):
             jj = self.Nx * (self.Ny-1) + ii
-            self.up[map.icjM[ii]] = self.up[ii]
-            self.up[map.icjP[jj]] = self.up[jj]
-            self.vp[map.icjM[ii]] = self.vp[ii]
-            self.vp[map.icjP[jj]] = self.vp[jj]
             self.pp[map.icjM[ii]] = self.pp[ii]
+            # self.pp[map.icjP[jj]] = 2.0 * self.pp[jj] - self.pp[map.icjM[jj]]
             self.pp[map.icjP[jj]] = self.pp[jj]
         for ii in range(self.Ny):
             jj = ii * self.Nx
             kk = (ii+1) * self.Nx - 1
-            self.up[map.iMjc[jj]] = self.up[jj]
+            self.pp[map.iMjc[jj]] = self.pp[jj]
+            self.pp[map.iPjc[kk]] = self.pp[kk]
+
+    def enforceVelocityBC(self, map, setting):
+        """ Enforce BC on velocity """
+        #   BC on ghost cells
+        for ii in range(self.Nx):
+            jj = self.Nx * (self.Ny-1) + ii
+            self.up[map.icjM[ii]] = self.up[ii]
+            self.up[map.icjP[jj]] = self.up[jj]
+            self.vp[jj] = self.vp[map.icjM[jj]] + (self.up[map.iMjc[jj]]-self.up[jj]) * self.Ax / self.Ay
+            self.vp[map.icjM[ii]] = self.vp[ii] + (self.up[ii]-self.up[map.iMjc[ii]]) * self.Ax / self.Ay
+            self.vp[map.icjP[jj]] = self.vp[jj]
+            # self.vp[map.icjP[jj]] = self.vp[jj] + (self.up[map.iMjc[jj]]-self.up[jj]) * self.Ax / self.Ay
+        for ii in range(self.Ny):
+            jj = ii * self.Nx
+            kk = (ii+1) * self.Nx - 1
+            self.up[kk] = 0.0
+            self.up[map.iMjc[jj]] = 0.0
             self.up[map.iPjc[kk]] = self.up[kk]
             self.vp[map.iMjc[jj]] = self.vp[jj]
             self.vp[map.iPjc[kk]] = self.vp[kk]
-            self.pp[map.iMjc[jj]] = self.pp[jj]
-            self.pp[map.iPjc[kk]] = self.pp[kk]
-            
+
     def buildMatrix(self, map, setting):
         """ Build sparse matrix A """
-        self.A = csc_matrix((self.Ni, self.Ni), dtype=float)
+        self.A = lil_matrix((self.Ni, self.Ni), dtype=float)
         for ii in range(self.Ni):
             self.A[ii,ii] = self.Gc
             #   For Neumann boundary
@@ -106,16 +133,31 @@ class Variable():
                 self.A[ii,ii+1] = -self.Gx
             #   For Direchlet boundary
             if map.icjM[ii] >= self.Ni:
-                self.A[ii,ii] = 1.0
-                self.B[ii] = self.pBC[0]
+                self.A[ii,ii] -= self.Gy
             else:
                 self.A[ii,ii-self.Nx] = -self.Gy
             if map.icjP[ii] >= self.Ni:
-                self.A[ii,ii] = 1.0
-                self.B[ii] = self.pBC[1]
+                self.A[ii,ii] -= self.Gy
+                # self.B[ii] += self.Gy * self.pBC[1]
             else:
                 self.A[ii,ii+self.Nx] = -self.Gy
-            
+        #   Enforce pressure BC
+        for ii in range(self.Ni):
+            if map.icjM[ii] >= self.Ni:
+                self.A[ii,ii] = 1.0
+                self.A[ii,ii+1] = 0.0
+                self.A[ii,ii+self.Nx] = 0.0
+                self.B[ii] = self.pBC[0]
+                if ii > 0:
+                    self.A[ii,ii-1] = 0.0
+            if map.icjP[ii] >= self.Ni:
+                self.A[ii,ii] = 1.0
+                self.A[ii,ii-1] = 0.0
+                self.A[ii,ii-self.Nx] = 0.0
+                self.B[ii] = self.pBC[1]
+                if ii < self.Ni-1:
+                    self.A[ii,ii+1] = 0.0
+
     def solve(self):
         """ Solve the linear system Ax = B """
         self.x = spsolve(self.A, self.B)
@@ -124,4 +166,3 @@ class Variable():
             self.vn[ii] = self.vp[ii]
             self.pn[ii] = self.pp[ii]
             self.pp[ii] = self.x[ii]
-        
